c8051f3636d0e5284336e52b0abf61e1c8771d78
otiGod <godwinotigo@gmail.com>
2024-02-10 12:03:45 +0100

    num  commit

--- numpyintro.py
+++ numpyintro.py
@@ -1,6 +1,3 @@
-#k="Hello world"
-#print(k)
-
 import numpy
 arr=numpy.array([1,2,3,4,5])
 print(arr)
@@ -53,3 +50,85 @@ import numpy as np
 arr=np.array([1,2,3,4],ndmin=5)
 print(arr)
 print("The number of array is increased to: ", arr.ndim)
+
+                                           ###NUMPY INDEXING
+
+import numpy as np 
+arr=np.array([1,2,3,4,5])
+print(arr.dtype)
+
+import numpy as np 
+arr=np.array(["Ben", "West","Dret"])
+print(arr.dtype) 
+
+import numpy as np 
+arr=np.array([1,2,3,4], dtype= "S")
+print(arr)
+print(arr.dtype)
+
+import numpy as np 
+arr=np.array([1,2,3,4], dtype= "i4")
+print(arr)
+print(arr.dtype)
+
+import numpy as np 
+arr=np.array([1.2,2.1,3.2,4.3])
+varr=arr.astype(int)
+print(varr)
+print(varr.dtype)
+
+import numpy as np  
+arr=np.array([1,2,3,4,5])
+varr=arr.astype("f")
+print(varr)
+print(varr.dtype)
+
+import numpy as np 
+arr=np.array([1,0,2,0,3,0,4])
+x=arr.astype(bool)
+c=arr.astype("b")
+print(x)
+print(x.dtype)
+print(c)
+print(c.dtype)
+
+                                           ###NUMPY DATATPYE
+
+import numpy as np 
+arr=np.array([1,2,3,4,5])
+print(arr.dtype)
+
+import numpy as np 
+arr=np.array(["Ben", "West","Dret"])
+print(arr.dtype) 
+
+import numpy as np 
+arr=np.array([1,2,3,4], dtype= "S")
+print(arr)
+print(arr.dtype)
+
+import numpy as np 
+arr=np.array([1,2,3,4], dtype= "i4")
+print(arr)
+print(arr.dtype)
+
+import numpy as np 
+arr=np.array([1.2,2.1,3.2,4.3])
+varr=arr.astype(int)
+print(varr)
+print(varr.dtype)
+
+import numpy as np  
+arr=np.array([1,2,3,4,5])
+varr=arr.astype("f")
+print(varr)
+print(varr.dtype)
+
+import numpy as np 
+arr=np.array([1,0,2,0,3,0,4])
+x=arr.astype(bool)
+c=arr.astype("b")
+print(x)
+print(x.dtype)
+print(c)
+print(c.dtype)
